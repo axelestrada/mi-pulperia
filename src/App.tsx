5b684dffdeef0b3243e717af1921252a9fc744cd
Axel Estrada <axele1524@gmail.com>
2025-08-19 12:49:04 -0600

    feat: add initial project setup with React, Tailwind CSS, and Electron
    - Added SVG assets for Electron and Vite.
    - Created main application structure with App component and Button component.
    - Implemented global styles using Tailwind CSS.
    - Configured TypeScript with strict settings and auto-imports for React components.
    - Set up Vite configuration for building the application with Electron integration.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import electron from "vite-plugin-electron/simple";
+
+import AutoImport from "unplugin-auto-import/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    AutoImport({
+      include: [
+         /\.[tj]sx?$/,
+      ],
+      imports: ["react"],
+      dirs: [
+        "./src/components/**/*",
+      ],
+      dts: './src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+      }
+    }),
+    electron({
+      main: {
+        entry: "electron/main.ts",
+      },
+      preload: {
+        input: path.join(__dirname, "electron/preload.ts"),
+      },
+      renderer: process.env.NODE_ENV === "test" ? undefined : {},
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
