f8e01c638ab94d4dfe437c6f404d2a1f90ba69f9
Axel Estrada <axele1524@gmail.com>
2025-12-26 14:51:39 -0600

    feat: update ESLint and Prettier configurations; standardize code formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import path from "node:path";
-import { defineConfig } from "vite";
+import path from 'node:path'
+import { defineConfig } from 'vite'
 
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import electron from "vite-plugin-electron/simple";
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import electron from 'vite-plugin-electron/simple'
 
-import Icons from "unplugin-icons/vite";
-import AutoImport from "unplugin-auto-import/vite";
+import Icons from 'unplugin-icons/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
@@ -14,38 +14,38 @@ export default defineConfig({
     react(),
     AutoImport({
       include: [/\.[tj]sx?$/, /\.[tj]s?$/],
-      imports: ["react", "react-router-dom"],
+      imports: ['react', 'react-router-dom'],
       dirs: [
-        "./src/components/**/*",
-        "./src/app/**/*",
-        "./src/components/ui/shadcn-io/**/*",
-        "./src/features/**/*",
-        "./src/shared/components/**/*",
-        "./src/shared/hooks/**/*",
-        "./src/shared/utils/**/*",
+        './src/components/**/*',
+        './src/app/**/*',
+        './src/components/ui/shadcn-io/**/*',
+        './src/features/**/*',
+        './src/shared/components/**/*',
+        './src/shared/hooks/**/*',
+        './src/shared/utils/**/*',
       ],
-      dts: "./src/auto-imports.d.ts",
+      dts: './src/auto-imports.d.ts',
       eslintrc: {
         enabled: true,
       },
     }),
     Icons({
-      compiler: "jsx",
-      jsx: "react",
+      compiler: 'jsx',
+      jsx: 'react',
     }),
     electron({
       main: {
-        entry: "electron/main.ts",
+        entry: 'electron/main.ts',
       },
       preload: {
-        input: path.join(__dirname, "electron/preload.ts"),
+        input: path.join(__dirname, 'electron/preload.ts'),
       },
-      renderer: process.env.NODE_ENV === "test" ? undefined : {},
+      renderer: process.env.NODE_ENV === 'test' ? undefined : {},
     }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
